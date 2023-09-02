9b06bd7bf62203d597fcef052c7695f54125966f
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-09-01 22:05:34 -0300

    Refatorando os códigos e melhorando o estilo em css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
   def index
     @extrato = Caixa.all
+    @extrato = @extrato.where("tipo like ?", "%#{params[:tipo]}%") if params[:tipo].present?
+
+    @valor_total = 0
+    @receita = 0
+    @despesa = 0
+
+    @extrato.each do |item|
+      if item.status == Caixa::DEBITO
+        @despesa += item.valor
+      else
+        @receita += item.valor
+      end
+    end
+    @valor_total = @receita - @despesa
   end
 end
--- app/models/caixa.rb
+++ app/models/caixa.rb
@@ -1,3 +1,4 @@
 class Caixa < ApplicationRecord
-  <%=()%>
+  CREDITO = 0
+  DEBITO = 1
 end
